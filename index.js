b546b0a4244e31ec27bbda55c89114b179c6485a
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-09 06:07:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ function happyHolidays(){
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(name){
-  return `Happy holidays` + ${name}!;
+function happyHolidaysTo(name) {
+  return "Happy holidays, you!"
 }
-
-happyHolidaysTo("Yaz");
