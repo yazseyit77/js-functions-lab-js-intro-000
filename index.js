cb25ee260d6f190de0e7bfb8ad1ec6a8e95a928d
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-09 06:17:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function happyHolidays(){
 function happyHolidaysTo(name) {
   return "Happy holidays, you!"
 }
+
+var holiday = "Independene Day";
+var name = "Men";
 function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!";
 }
